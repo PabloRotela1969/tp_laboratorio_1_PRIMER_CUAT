fe1392b542b379560d8caa874e59483b6a33ec79
unknown <pablo.rotela@gmail.com>
2020-04-06 01:14:31 -0300

    Agregado de factorial y documentado

--- TP1/Funciones.h
+++ TP1/Funciones.h
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* biblioteca de funciones matematicas */
 
+
+
+//recibe dos enteros y entrega uno como suma
 int suma(int primero,int segundo)
 {
     return primero + segundo;
 }
-
+//recibe dos enteros y entrega uno como resta
 int resta(int primero,int segundo)
 {
     return primero - segundo;
 }
-
+//recibe dos enteros y entrega uno como producto
 int multiplicacion(int primero, int segundo)
 {
     return primero * segundo;
 }
-
+//recibe dos enteros y entrega un flotante como cociente
 float division(int primero, int segundo)
 {
    return (float)primero / segundo;
 
+}
+
+//recibe un entero y acumula los sucesivos productos desde el 1 al numero
+//entregando el acumulado como entero
+int factorial(int primero)
+{
+    int i;
+    int acumulado = 1;
+    for(i = 1; i <= primero ; i++)
+    {
+        acumulado *= i;
+    }
+    return acumulado;
 }
--- TP1/main.c
+++ TP1/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "Funciones.h"
+#include "Funciones.h" /* librería de funciones matemáticas */
 
-float resulta;
 int main()
 {
+    // números que ingresa el usuario
     int numero1;
     int numero2;
+    // carácter que representa la operación ingresada por el usuario
     char operacion;
-
+    // variable bandera usada para validar ingreso de operación
     bool mal = true;
 
 
@@ -19,11 +20,12 @@ int main()
     printf("\n Ingrese el segundo numero : ");
     scanf("%d", &numero2);
 
+    // validación de ingreso de función y cálculo de función
     do
     {
         mal = false;
         fflush(stdin);
-        printf("\n Elija una operacion entre las siguientes : +, - , * y / ");
+        printf("\n Elija una operacion entre las siguientes : +, - , * , / , ! : ");
         scanf("%c", &operacion);
 
         switch(operacion)
@@ -39,6 +41,7 @@ int main()
             printf(" %d %c %d = %d \n" , numero1,operacion,numero2,multiplicacion(numero1,numero2));
             break;
         case '/':
+            // valido que el denominador sea distinto de cero
             if (numero2 > 0)
             {
                 printf(" %d %c %d = %f \n" , numero1,operacion,numero2,division(numero1,numero2));
@@ -47,14 +50,18 @@ int main()
             {
                 printf("No se puede dividir por cero \n");
             }
-
-
+            break;
+        case '!':
+            printf(" %c%d = %d y el " , operacion,numero1,factorial(numero1));
+            printf(" %c%d = %d \n" , operacion,numero2,factorial(numero2));
             break;
         default:
             mal = true;
+            // no se ingresó ningún caracter de función contemplado, muestro error
             printf("Tenga en bien ingresar los signos de operacion requeridos");
             break;
         }
+    // si no se ingresó ninguno de los caracteres, vuelvo a iterar y pido volver a ingresar
     }while(mal);
 
 
